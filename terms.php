9f62ad09d5bc6feef996517672afd566f0374613
kylekce <kylekce@gmail.com>
2022-07-03 15:20:50 +0800

    register_customer page(please read the comments)

---
+++ terms.php
@@ -0,0 +1,13 @@
+<p align = "center">
+The quick brown fox jumps over the lazy dog. The quick
+brown fox jumps over the lazy dog. The quick brown fox
+jumps over the lazy dog. The quick brown fox jumps over
+the lazy dog. The quick brown fox jumps over the lazy dog.
+The quick brown fox jumps over the lazy dog. The quick
+brown fox jumps over the lazy dog. The quick brown fox
+jumps over the lazy dog. The quick brown fox jumps over
+the lazy dog. The quick brown fox jumps over the lazy dog.
+The quick brown fox jumps over the lazy dog. The quick
+brown fox jumps over the lazy dog.
+</p>
+<br><a href = "register_customer.php">Return to Sign-Up Page</a>
