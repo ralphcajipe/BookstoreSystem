99b75728d734329b56dcc11421dc3e2009b2534f
Ralph Cajipe <ralphcajipe@gmail.com>
2022-07-12 12:44:41 +0800

    Remove Shop Now! p text

--- home.php
+++ home.php
@@ -3,7 +3,6 @@
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
             <h1 class="display-4 fw-bolder">The best computer technology books at your fingertips.</h1>
-            <p class="lead fw-normal text-white-50 mb-0">Shop Now!</p>
         </div>
     </div>
 </header>
